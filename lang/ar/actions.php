341cde8ce2856204a8193a1d7aea174f84609d98
Mosaab Emam <mosaab.emam123@gmail.com>
2024-04-07 09:57:27 +0000

    feat(price_offer): replace dedicated page with action + modal

---
+++ lang/ar/actions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'create_price_offer_pdf' => 'إنشاء عرض سعر'
+];
--- resources/views/filament/pages/input-to-p-d-f.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-panels::page >
-    <form wire:submit="create">
-        {{ $this->form }}
-
-        <x-filament::button type="submit">
-            {{ __('انشاء الملف') }}
-        </x-filament::button>
-    </form>
-</x-filament-panels::page>

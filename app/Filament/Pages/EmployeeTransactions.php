7c284b97fadb705d757e26c745a784c5531d4d27
Mosaab Emam <mosaab.emam123@gmail.com>
2024-04-15 13:27:34 +0000

    feat(employee-transactions): review
    - code formatting
    - enable excel export
    - toggleable columns
    - review filters

--- app/Filament/Pages/EmployeeTransactions.php
+++ app/Filament/Pages/EmployeeTransactions.php
@@ -2,19 +2,16 @@
 
 namespace App\Filament\Pages;
 
-use App\Exports\GenericExport;
 use Carbon\Carbon;
-use Excel;
 use Filament\Forms\Components\DatePicker;
 use Filament\Pages\Page;
-use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
-use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class EmployeeTransactions extends Page implements HasTable
 {
@@ -52,23 +49,39 @@ class EmployeeTransactions extends Page implements HasTable
     public function table(Table $table): Table
     {
         return $table
-            ->query(\App\Models\Evaluation\EvaluationEmployee::query()->withCount('transactionpreviewer','transactionincome','transactionreview'))
+            ->query(\App\Models\Evaluation\EvaluationEmployee::query()->withCount('transactionpreviewer', 'transactionincome', 'transactionreview'))
             ->columns([
-                TextColumn::make('title')->label(__('admin.Title'))->searchable(),
-                TextColumn::make('total')->label('إجمالى المعاملات'),
+                TextColumn::make('title')
+                    ->label(__('admin.Title'))
+                    ->searchable()
+                    ->toggleable(),
+                TextColumn::make('total')
+                    ->label('إجمالى المعاملات')
+                    ->toggleable()
+                    ->sortable(),
                 TextColumn::make('transactionpreviewer_count')
-                    ->label(' المعاينات'),
+                    ->label(' المعاينات')
+                    ->toggleable()
+                    ->sortable(),
                 TextColumn::make('transactionincome_count')
-                    ->formatStateUsing(fn(string $state) => $state * .5)->label('الإدخال'),
+                    ->formatStateUsing(fn (string $state) => $state * .5)
+                    ->label('الإدخال')
+                    ->toggleable()
+                    ->sortable(),
                 TextColumn::make('transactionreview_count')
-                    ->formatStateUsing(fn(string $state) => $state * .5)->label('المراجعة')
-
+                    ->formatStateUsing(fn (string $state) => $state * .5)
+                    ->label('المراجعة')
+                    ->toggleable()
+                    ->sortable(),
             ])
             ->filters([
                 Filter::make('created_at')
                     ->form([
-                        DatePicker::make('created_from')->label(__('من تاريخ')),
-                    ])->baseQuery(function (Builder $query, array $data): Builder {
+                        DatePicker::make('created_from')
+                            ->label(__('من تاريخ'))
+                            ->native(false),
+                    ])
+                    ->baseQuery(function (Builder $query, array $data): Builder {
                         return $query
                             ->when(
                                 $data['created_from'],
@@ -82,20 +95,22 @@ class EmployeeTransactions extends Page implements HasTable
                             );
                     })
                     ->indicateUsing(function (array $data): ?string {
-                        if (! $data['created_from']) {
+                        if (!$data['created_from'])
                             return null;
-                        }
 
                         return 'Created from ' . Carbon::parse($data['created_from'])->toFormattedDateString();
                     }),
                 Filter::make('created_until')
                     ->form([
-                        DatePicker::make('created_until')->label(__('قبل تاريخ')),
-                    ])->baseQuery(function (Builder $query, array $data): Builder {
+                        DatePicker::make('created_until')
+                            ->label(__('قبل تاريخ'))
+                            ->native(false),
+                    ])
+                    ->baseQuery(function (Builder $query, array $data): Builder {
                         return $query
                             ->when(
                                 $data['created_until'],
-                                function (Builder $query, $date): Builder  {
+                                function (Builder $query, $date): Builder {
                                     return $query->withCount(['transactionpreviewer' => function ($q) use ($date) {
                                         $q->whereDate('created_at', '<=', $date);
                                     }])->withCount(['transactionincome' => function ($q) use ($date) {
@@ -103,44 +118,21 @@ class EmployeeTransactions extends Page implements HasTable
                                     }])->withCount(['transactionreview' => function ($q) use ($date) {
                                         $q->whereDate('created_at', '<=', $date);
                                     }]);
-                                } ,
+                                },
                             );
-                    })->indicateUsing(function (array $data): ?string {
-                        if (! $data['created_until']) {
+                    })
+                    ->indicateUsing(function (array $data): ?string {
+                        if (!$data['created_until'])
                             return null;
-                        }
 
                         return 'Created until ' . Carbon::parse($data['created_until'])->toFormattedDateString();
                     }),
-            ], layout: FiltersLayout::AboveContent)
+            ])
             ->actions([
                 // ...
             ])
             ->bulkActions([
-                BulkAction::make('export_excel')->label(__('تصدير اقفالات الموظفين'))
-                    ->icon('heroicon-m-arrow-down-tray')
-                    ->action(function (\Illuminate\Support\Collection $records)  {
-
-                        $columns =  [
-                            __('المراجعة'),
-                            __('الادخال'),
-                            __('المعاينات'),
-                            __('إجمالى المعاملات'),
-                            __('admin.Title'),
-                        ];
-
-                        $fields = [
-                            'transactionreview_count',
-                            'transactionincome_count',
-                            'transactionpreviewer_count',
-                            'total',
-                            'title',
-                        ];
-
-                        $exports = new GenericExport($records,$columns,$fields);
-                        $fileName = 'EmployeeTransactions_' . time() . '.xlsx';
-                        return Excel::download($exports,$fileName);
-                    }),
+                ExportBulkAction::make(),
             ]);
     }
 }

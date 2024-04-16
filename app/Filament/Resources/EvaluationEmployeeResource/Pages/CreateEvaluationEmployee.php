24e9c65675652ea57a58ef47796ef3e70fb27fda
Mosaab Emam <mosaab.emam123@gmail.com>
2024-04-16 06:13:52 +0000

    feat(evaluation-employee): redirect to index after creating new record

--- app/Filament/Resources/EvaluationEmployeeResource/Pages/CreateEvaluationEmployee.php
+++ app/Filament/Resources/EvaluationEmployeeResource/Pages/CreateEvaluationEmployee.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\EvaluationEmployeeResource\Pages;
 
 use App\Filament\Resources\EvaluationEmployeeResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEvaluationEmployee extends CreateRecord
@@ -11,4 +10,9 @@ class CreateEvaluationEmployee extends CreateRecord
     protected static string $resource = EvaluationEmployeeResource::class;
 
     protected static string | array $routeMiddleware = 'checkPermission:evaluation-employees.index';
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
 }

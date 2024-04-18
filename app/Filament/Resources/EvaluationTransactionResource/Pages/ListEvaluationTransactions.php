6faa9432b6a6d6188a919c35b063a41abaefe3fa
Mosaab Emam <mosaab.emam123@gmail.com>
2024-04-18 13:22:55 +0000

    feat(et): add status filtering tabs

--- app/Filament/Resources/EvaluationTransactionResource/Pages/ListEvaluationTransactions.php
+++ app/Filament/Resources/EvaluationTransactionResource/Pages/ListEvaluationTransactions.php
@@ -4,13 +4,15 @@ namespace App\Filament\Resources\EvaluationTransactionResource\Pages;
 
 use App\Filament\Resources\EvaluationTransactionResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListEvaluationTransactions extends ListRecords
 {
     protected static string $resource = EvaluationTransactionResource::class;
 
-   /* protected static string $view = 'filament.pages.evaluation-transactions';*/
+    /* protected static string $view = 'filament.pages.evaluation-transactions';*/
 
     protected static string | array $routeMiddleware = 'checkPermission:evaluation-transactions.index';
 
@@ -21,5 +23,24 @@ class ListEvaluationTransactions extends ListRecords
         ];
     }
 
-
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(__('admin.evaluation-transactions.statuses.all')),
+            '0' => Tab::make(__('admin.evaluation-transactions.statuses.0'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 0)),
+            '1' => Tab::make(__('admin.evaluation-transactions.statuses.1'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 1)),
+            '2' => Tab::make(__('admin.evaluation-transactions.statuses.2'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 2)),
+            '3' => Tab::make(__('admin.evaluation-transactions.statuses.3'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 3)),
+            '4' => Tab::make(__('admin.evaluation-transactions.statuses.4'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 4)),
+            '5' => Tab::make(__('admin.evaluation-transactions.statuses.5'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 5)),
+            '6' => Tab::make(__('admin.evaluation-transactions.statuses.6'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 6)),
+        ];
+    }
 }

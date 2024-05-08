2a36a6b665fdc3167b17d25acb2a31dbadda8ed5
Mosaab Emam <mosaab.emam123@gmail.com>
2024-05-08 14:49:10 +0000

    chore(about): review about resource

--- app/Filament/Resources/AboutResource.php
+++ app/Filament/Resources/AboutResource.php
@@ -3,9 +3,8 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\AboutResource\Pages;
-use App\Filament\Resources\AboutResource\RelationManagers;
-use App\Models\About;
 use App\Models\Content;
+use App\Models\Scopes\ActiveScope;
 use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Forms\Components\DatePicker;
@@ -15,8 +14,12 @@ use Filament\Tables;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
+#[ScopedBy(ActiveScope::class)]
 class AboutResource extends Resource
 {
     protected static ?string $model = Content::class;
@@ -29,7 +32,7 @@ class AboutResource extends Resource
 
     public static function getEloquentQuery(): Builder
     {
-        return parent::getEloquentQuery()->about();
+        return static::getModel()::withoutGlobalScope(ActiveScope::class)->about()->orderBy('position');
     }
 
     public static function getModelLabel(): string
@@ -55,18 +58,43 @@ class AboutResource extends Resource
         return __('admin.GeneralSettings');
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            Infolists\Components\Split::make([
+                Infolists\Components\Section::make([
+                    Infolists\Components\Grid::make(2)->schema([
+                        Infolists\Components\TextEntry::make('title')
+                            ->label(__('admin.Title')),
+                        Infolists\Components\TextEntry::make('position')
+                            ->label(__('admin.Position')),
+                        Infolists\Components\IconEntry::make('active')
+                            ->label(__('admin.Publish'))
+                            ->boolean(),
+                    ])
+                ]),
+                Infolists\Components\Section::make([
+                    Infolists\Components\TextEntry::make('created_at')
+                        ->label(__('admin.CreationDate'))
+                        ->dateTime(),
+                    Infolists\Components\TextEntry::make('updated_at')
+                        ->label(__('admin.LastUpdate'))
+                        ->dateTime(),
+                ])->grow(false)
+            ])->from('md')->columnSpanFull()
+        ]);
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('title')->label(__('admin.Title'))
+                Forms\Components\TextInput::make('title')
+                    ->label(__('admin.Title'))
                     ->maxLength(255)
                     ->required(),
-                Forms\Components\TextInput::make('position')->label(__('admin.Position'))
-                    ->numeric()
-                    ->default(0)
-                    ->required(),
-                Forms\Components\Toggle::make('active')->label(__('admin.Publish'))
+                Forms\Components\Toggle::make('active')
+                    ->label(__('admin.Publish'))
                     ->required(),
             ]);
     }
@@ -74,67 +102,30 @@ class AboutResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->reorderable('position')
             ->columns([
-                Tables\Columns\TextColumn::make('id')->label('#'),
-                Tables\Columns\TextColumn::make('title')->label(__('admin.Title'))
+                Tables\Columns\TextColumn::make('title')
+                    ->label(__('admin.Title'))
+                    ->toggleable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('position')->label(__('admin.Position'))
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('active')->label(__('admin.Publish'))
+                Tables\Columns\IconColumn::make('active')
+                    ->label(__('admin.Publish'))
+                    ->toggleable()
                     ->boolean(),
-                Tables\Columns\TextColumn::make('created_at')->label(__('admin.CreationDate'))
-                    ->dateTime()
-                    ->sortable()
 
             ])
             ->filters([
-                Filter::make('created_at')
-                    ->form([
-                        DatePicker::make('created_from')
-                            ->label(__('من تاريخ'))
-                            ->native(false),
-                    ])
-                    ->query(function (Builder $query, array $data): Builder {
-                        return $query
-                            ->when(
-                                $data['created_from'],
-                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
-                            );
-                    })->indicateUsing(function (array $data): ?string {
-                        if (!$data['created_from']) {
-                            return null;
-                        }
-
-                        return 'Created from ' . Carbon::parse($data['created_from'])->toFormattedDateString();
-                    }),
-                Filter::make('created_until')
-                    ->form([
-                        DatePicker::make('created_until')
-                            ->label(__('قبل تاريخ'))
-                            ->native(false),
-                    ])->query(function (Builder $query, array $data): Builder {
-                        return $query
-                            ->when(
-                                $data['created_until'],
-                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
-                            );
-                    })->indicateUsing(function (array $data): ?string {
-                        if (!$data['created_until']) {
-                            return null;
-                        }
-
-                        return 'Created until ' . Carbon::parse($data['created_until'])->toFormattedDateString();
-                    }),
-                Tables\Filters\TernaryFilter::make('active')->label(__('admin.Publish')),
-            ], layout: Tables\Enums\FiltersLayout::AboveContent)
+                Tables\Filters\TernaryFilter::make('active')
+                    ->label(__('admin.Publish')),
+            ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                ExportBulkAction::make(),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
@@ -149,6 +140,7 @@ class AboutResource extends Resource
     {
         return [
             'index' => Pages\ListAbouts::route('/'),
+            'view' => Pages\ViewAbout::route('/{record}'),
             'create' => Pages\CreateAbout::route('/create'),
             'edit' => Pages\EditAbout::route('/{record}/edit'),
         ];

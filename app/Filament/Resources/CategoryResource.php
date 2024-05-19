6fe266353e357e51cc7ea14a25dee2675074550d
Mosaab Emam <mosaab.emam123@gmail.com>
2024-05-19 13:23:04 +0000

    fix(categories): actually ignore deleted create page

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -181,7 +181,7 @@ class CategoryResource extends Resource
         return [
             'index' => Pages\ListCategories::route('/'),
             'view' => Pages\ViewCategory::route('/{record}'),
-            'create' => Pages\CreateCategory::route('/create'),
+            // 'create' => Pages\CreateCategory::route('/create'),
             /* 'edit' => Pages\EditCategory::route('/{record}/edit'),*/
         ];
     }

7dbcb7496996541e803014676fbd4d5c7a5f9a41
Mosaab <mosaab.emam123@outlook.com>
2025-01-01 07:29:50 -0500

    fix: remove unused files

--- resources/js/xPages/nestedPages/blogDetailsPage/relatedTopics/RelatedTopicsSlider.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from "react";
-import RelatedSlidBox from "./RelatedSlidBox";
-
-const RelatedTopicsSlider = () => {
-	const swiperRef = useRef(null);
-	return <RelatedSlidBox swiperRef={swiperRef} />;
-};
-
-export default RelatedTopicsSlider;

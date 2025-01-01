b1d5b886eaba8cfee34c22a11d11d2bbd1bb57d6
Mosaab <mosaab.emam123@outlook.com>
2025-01-01 11:27:30 -0500

    chore: testing navigation waters

--- resources/js/components/ButtonsBox.jsx
+++ resources/js/components/ButtonsBox.jsx
@@ -1,35 +1,42 @@
-import React from "react";
 import Button from "./button/Button";
 
 const ButtonsBox = ({
-	icon,
-	radius = "right",
-	btnWidth = "sm:w-[140px] lg:w-[130px] xl:w-[150px]",
-	outLinButtonOnClick,
-	primaryButtonOnClick,
-	flexDirection = "flex-row",
-	gap = "sm:gap-8 lg:gap-2 xl:gap-4",
-	primaryBtnContent,
-	outlineBtnContent,
+    icon,
+    radius = "right",
+    btnWidth = "sm:w-[140px] lg:w-[130px] xl:w-[150px]",
+    outLinButtonOnClick,
+    primaryButtonOnClick,
+    flexDirection = "flex-row",
+    gap = "sm:gap-8 lg:gap-2 xl:gap-4",
+    primaryBtnContent,
+    outlineBtnContent,
+    secondaryBtnHref,
 }) => {
-	return (
-		<div
-			className={`w-full flex items-start  md:justify-end justify-center ${flexDirection} ${gap}`}>
-			<Button
-				onClick={outLinButtonOnClick}
-				className={`sm:h-[48px] lg:h-[46px] xl:h-[48px] ${btnWidth}`}
-				variant='out-line'>
-				{outlineBtnContent}
-			</Button>
-			<Button
-				onClick={primaryButtonOnClick}
-				className={`sm:h-[48px] lg:h-[46px] xl:h-[48px] ${btnWidth}`}
-				radius={radius}>
-				{icon && <span className='w-5 h-5'>{icon}</span>}
-				{primaryBtnContent}
-			</Button>
-		</div>
-	);
+    return (
+        <div
+            className={`w-full flex items-start  md:justify-end justify-center ${flexDirection} ${gap}`}
+        >
+            <Button
+                onClick={!secondaryBtnHref ? outLinButtonOnClick : null}
+                className={`sm:h-[48px] lg:h-[46px] xl:h-[48px] ${btnWidth}`}
+                variant="out-line"
+            >
+                {secondaryBtnHref ? (
+                    <a href={secondaryBtnHref}>{outlineBtnContent}</a>
+                ) : (
+                    outlineBtnContent
+                )}
+            </Button>
+            <Button
+                onClick={primaryButtonOnClick}
+                className={`sm:h-[48px] lg:h-[46px] xl:h-[48px] ${btnWidth}`}
+                radius={radius}
+            >
+                {icon && <span className="w-5 h-5">{icon}</span>}
+                {primaryBtnContent}
+            </Button>
+        </div>
+    );
 };
 
 export default ButtonsBox;
--- resources/js/components/aboutSection/LeftContent.jsx
+++ resources/js/components/aboutSection/LeftContent.jsx
@@ -1,55 +1,56 @@
 import React from "react";
 
 // components
-import AboutGoals from "./AboutGoals";
 import ButtonsBox from "../ButtonsBox";
+import AboutGoals from "./AboutGoals";
 
 // icons
 import { DownloadIcon } from "../../assets/icons";
 
 // data
-import { aboutData } from "../../data";
 import { useNavigate } from "react-router-dom";
 import { samplePdf } from "../../assets/pdf-docs";
+import { aboutData } from "../../data";
 
 const LeftContent = () => {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	const handleDownload = () => {
-		const link = document.createElement("a");
+    const handleDownload = () => {
+        const link = document.createElement("a");
 
-		link.href = samplePdf;
-		link.download = `${"تقرير السوق العقاري 2024"}.pdf`;
-		link.click();
-	};
+        link.href = samplePdf;
+        link.download = `${"تقرير السوق العقاري 2024"}.pdf`;
+        link.click();
+    };
 
-	return (
-		<section className='flex flex-col justify-start md:self-end self-start items-start w-full lg:w-[387px] overflow-hidden'>
-			{aboutData.map((item, index) => (
-				<AboutGoals
-					index={index}
-					key={item.id}
-					img={item.img}
-					title={item.title}
-					desc={item.desc}
-				/>
-			))}
+    return (
+        <section className="flex flex-col justify-start md:self-end self-start items-start w-full lg:w-[387px] overflow-hidden">
+            {aboutData.map((item, index) => (
+                <AboutGoals
+                    index={index}
+                    key={item.id}
+                    img={item.img}
+                    title={item.title}
+                    desc={item.desc}
+                />
+            ))}
 
-			<ButtonsBox
-				radius={"left"}
-				btnWidth='w-full lg:w-full xl:w-[387px] '
-				gap={"gap-[10px]"}
-				flexDirection={"flex-col-reverse"}
-				icon={<DownloadIcon />}
-				outlineBtnContent={"عرض المزيد من التقارير المعتمدة"}
-				primaryBtnContent={"تحميل تقرير السوق العقاري 2024"}
-				primaryButtonOnClick={handleDownload}
-				outLinButtonOnClick={() => {
-					navigate("/about-us");
-				}}
-			/>
-		</section>
-	);
+            <ButtonsBox
+                radius={"left"}
+                btnWidth="w-full lg:w-full xl:w-[387px] "
+                gap={"gap-[10px]"}
+                flexDirection={"flex-col-reverse"}
+                icon={<DownloadIcon />}
+                outlineBtnContent={"عرض المزيد من التقارير المعتمدة"}
+                primaryBtnContent={"تحميل تقرير السوق العقاري 2024"}
+                primaryButtonOnClick={handleDownload}
+                outLinButtonOnClick={() => {
+                    navigate("/about-us");
+                }}
+                secondaryBtnHref="/about-us"
+            />
+        </section>
+    );
 };
 
 export default LeftContent;

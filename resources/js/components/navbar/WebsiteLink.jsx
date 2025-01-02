d91bab4f1a4b0d445daeef31fe8e0fb145c275a6
Mosaab <mosaab.emam123@outlook.com>
2025-01-02 06:35:48 -0500

    fix: keep using client routing until solving layout issue

--- resources/js/components/navbar/WebsiteLink.jsx
+++ resources/js/components/navbar/WebsiteLink.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function WebsiteLink({ to = "/", children }) {
     const location = useLocation();
@@ -7,7 +7,7 @@ export default function WebsiteLink({ to = "/", children }) {
     const isActive = location.pathname === to;
 
     return (
-        <a
+        <Link
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
             className={`w-full h-full flex md:justify-center justify-start md:items-center items-center xl:gap-[6px] lg:gap-1 gap-[10px] text-sm font-normal md:border border-b pb-6 md:py-2 transition-all duration-400 ease-in-out 
@@ -16,12 +16,12 @@ export default function WebsiteLink({ to = "/", children }) {
           ? "text-primary-500 font-medium md:border-[#ECECEC] border-primary-600"
           : "text-Gray-scale-02 hover:text-primary-500 hover:font-medium border-[#ECECEC]"
   }`}
-            href={to}
+            to={to}
         >
             {(isActive || isHovered) && (
                 <span className="w-[6px] h-[6px] rounded-full bg-primary-500 transition-transform duration-400 ease-in-out scale-100"></span>
             )}
             {children}
-        </a>
+        </Link>
     );
 }

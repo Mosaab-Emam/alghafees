c5fd20004f390a92f699680ff4f414614285a1f4
Mosaab <mosaab.emam123@outlook.com>
2025-01-15 07:45:12 -0500

    fix(static-content): about -us & our-services static contents
    - Seed with actual content

--- database/seeders/OurServicesStaticContentSeeder.php
+++ database/seeders/OurServicesStaticContentSeeder.php
@@ -14,11 +14,11 @@ class OurServicesStaticContentSeeder extends Seeder
     public function run(): void
     {
         OurServicesStaticContent::create([
-            'small_top_title' => '',
-            'main_top_title' => '',
-            'main_title' => '',
-            'main_description' => '',
-            'services_title' => '',
+            'small_top_title' => 'خدماتنا',
+            'main_top_title' => 'حلول عقارية',
+            'main_title' => 'استفد من $$خدماتنا العقارية$$ المتميزة لدعم قراراتك الذكية',
+            'main_description' => 'نقدم في شركة صالح الغفيص أفضل الخدمات للمستفيدين في جميع مناطق المملكة.',
+            'services_title' => 'خدماتنا العقارية',
             // Placeholder for services and their subitems can be added here
         ]);
     }

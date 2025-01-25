506f5e3a32f1afe9d2216e0c3a13f7d98e4f3f9c
Mosaab <mosaab.emam123@outlook.com>
2025-01-25 11:20:10 -0500

    fix(responsive): Website links hotfix

--- resources/js/components/footer/AllRightsReserved.jsx
+++ resources/js/components/footer/AllRightsReserved.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const AllRightsReserved = () => {
     const date = new Date();
     return (
-        <div className="mb-8 flex justify-center items-center ">
+        <div className="my-8 flex justify-center items-center ">
             <p className="regular-b1 text-Gray-scale-02 ">
                 جميع الحقوق محفوظة لدى{" "}
                 <span className="text-primary-600 regular-b1">

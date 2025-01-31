227a11339b68eb656d7ea7702943ed6c721b53ab
Mosaab <mosaab.emam123@outlook.com>
2025-01-31 10:53:41 -0500

    feat(responsive): Responsify Join us page

--- resources/js/Pages/joinUs/JoinUs.tsx
+++ resources/js/Pages/joinUs/JoinUs.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/Container";
 import {
     Bullet,
     OurClientsShape,
@@ -12,28 +13,32 @@ import JoinUsTextBox from "./JoinUsTextBox";
 const JoinUs = () => (
     <>
         <PageTopSection title={"انضم الينا"} description={"شاركنا النجاح"} />
-        <section className="container md:mt-[315px] mt-[6rem] xl:mb-[500px] lg:mb-[450px] mb-[38rem]">
-            <div className="relative md:block flex flex-col items-center  ">
-                <JoinUsBgImage />
-                <JoinUsForm />
-                <JoinUsTextBox />
-            </div>
+        <Container>
+            <section className="md:mt-[8.5rem] lg:mt-[315px] mt-[6rem] xl:mb-[500px] lg:mb-[450px] mb-[38rem]">
+                <div className="relative md:block flex flex-col items-center">
+                    <JoinUsBgImage />
+                    <JoinUsForm />
+                    <JoinUsTextBox />
+                </div>
 
-            <Bullet
-                blue={true}
-                position={
-                    "2xl:left-[6.8rem] xl:left-[9.5rem] xl:top-[69.5rem] lg:left-[4.7rem] lg:top-[67.7rem] -left-2 top-[36.7rem] z-10"
-                }
-            />
-            <SalehNameEnglishShape
-                position={
-                    "md:left-[-93px] left-[-31px] md:-bottom-[50rem] bottom-[106rem]"
-                }
-            />
-            <OurClientsShape
-                position={"md:flex hidden right-[-6rem] -bottom-[29rem] z-[-1]"}
-            />
-        </section>
+                <Bullet
+                    blue={true}
+                    position={
+                        "2xl:left-[6.8rem] xl:left-[9.5rem] xl:top-[69.5rem] lg:left-[4.7rem] lg:top-[67.7rem] -left-2 top-[36.7rem] z-10"
+                    }
+                />
+                <SalehNameEnglishShape
+                    position={
+                        "md:left-[-93px] left-[-31px] md:-bottom-[50rem] bottom-[106rem]"
+                    }
+                />
+                <OurClientsShape
+                    position={
+                        "md:flex hidden right-[-6rem] -bottom-[29rem] z-[-1]"
+                    }
+                />
+            </section>
+        </Container>
     </>
 );
 

9764b70857565c75331eb37d381c330cd18332a5
Mosaab <mosaab.emam123@outlook.com>
2025-04-11 18:23:12 -0400

    chore: ditch work tracker livewire component

--- resources/views/filament/pages/statistics.blade.php
+++ resources/views/filament/pages/statistics.blade.php
@@ -1,7 +1,2 @@
 <x-filament-panels::page>
-    <div class="w-full grid md:grid-cols-2 gap-2 md:gap-4" dir="rtl">
-        <div class="col-span-full w-full mb-5">
-            <livewire:work-trackers-widget />
-        </div>
-    </div>
 </x-filament-panels::page>

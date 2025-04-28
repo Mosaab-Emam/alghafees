4c3a0b1f374747c3d9869cfffaf4238209532008
Mosaab <mosaab.emam123@outlook.com>
2025-04-28 10:42:30 -0400

    feat: extend contents on xl screens

--- resources/js/Pages/layout/Layout.tsx
+++ resources/js/Pages/layout/Layout.tsx
@@ -5,7 +5,7 @@ import NewLetter from "../../components/footer/newsLetter/NewsLetter.jsx";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col min-h-screen relative overflow-hidden max-w-[1440px] mx-auto">
+        <div className="flex flex-col min-h-screen relative overflow-hidden mx-auto">
             <ScrollProgress />
             <Navbar />
             <main className="flex-grow">{children}</main>

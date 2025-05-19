2fb2013d70687eacb7240cf01800063c895a6577
Mosaab <mosaab.emam123@outlook.com>
2025-05-19 18:39:26 -0400

    fix: button radius styles in Safari

--- resources/js/components/button/Button.jsx
+++ resources/js/components/button/Button.jsx
@@ -14,11 +14,17 @@ const Button = ({
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={`btn ${
-                radius === "right" ? "btn-rtl-radius" : "btn-ltr-radius"
-            } ${
-                variant === "primary" ? "btn-primary " : "btn-outline"
-            } ${className}`}
+            className={`flex min-w-[150px] h-[50px] py-[15px] px-[44px] justify-center items-center gap-[10px] border-[2px] border-primary-600 text-primary-600 transition-all duration-200 ease-in-out
+                ${
+                    variant === "primary"
+                        ? "bg-primary-600 text-white"
+                        : "bg-white text-primary-600"
+                }
+                ${
+                    radius === "right"
+                        ? "rounded-tr-[50px] rounded-bl-[50px]"
+                        : "rounded-tl-[50px] rounded-br-[50px]"
+                }  ${className}`}
         >
             {children}
         </button>

9bfbdb89fadf950d9698b2d7bb6e92f0555189b2
Mosaab <mosaab.emam123@outlook.com>
2025-07-24 09:38:43 -0400

    chore: add google tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,18 @@
 <html lang="ar" dir="rtl" style="overflow-x: hidden;">
 
 <head>
+    <!-- Should be as hight in <head> as possible -->
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            }); var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-MRPWJN6X');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-E5E2FZ4TMQ"></script>
     <script>
@@ -68,6 +80,10 @@
 </head>
 
 <body class="font-sans antialiased">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRPWJN6X" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @inertia
 
 </body>
